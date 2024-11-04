635192f73feedb47c71ee97c18f2228e355e7bf5
mohammedgodil <mohammed.godil@student.howest.be>
2024-11-04 11:29:15 +0100

    readme file and other chnages

--- room1/quiz.js
+++ room1/quiz.js
@@ -1,19 +1,17 @@
-document.getElementById('quiz-form').addEventListener('submit', (event) => {
-    event.preventDefault();
+document.getElementById("quiz-form").addEventListener("submit", (event) => {
+  event.preventDefault();
 
+  const answerInput = document.getElementById("answer");
+  const errorMessage = document.getElementById("errorMessage");
 
-    const answerInput = document.getElementById('answer');
-    const errorMessage = document.getElementById('errorMessage');
+  const correctAnswer = "8";
 
-
-    const correctAnswer = "8";
-
-    if (answerInput.value.trim() === correctAnswer) {
-        alert("Correct! You may proceed to the next room.");
-        window.location.href = "./room2.html";
-    } else {
-        errorMessage.style.display = 'block';
-        answerInput.style.borderColor = '#ff007b';
-        answerInput.style.color = '#ff007b';
-    }
+  if (answerInput.value.trim() === correctAnswer) {
+    alert("Correct! You may proceed to the next room.");
+    window.location.href = "../room2/index.html";
+  } else {
+    errorMessage.style.display = "block";
+    answerInput.style.borderColor = "#ff007b";
+    answerInput.style.color = "#ff007b";
+  }
 });
